df4e727da715c6b66bcd5c1fe247e3e4ed1fcc7e
Hongda Li <lhddragon87@gmail.com>
2016-04-08 12:56:05 -0400

    add form validation

--- client/components/prequalifyPanel/prequalifyPanel.controller.js
+++ client/components/prequalifyPanel/prequalifyPanel.controller.js
@@ -2,58 +2,77 @@
 
 angular.module('ng-gulp-hapi')
 	.controller('PrequalifyCtrl', function($scope, $http, $state, businessType, dataService, ModalService) {
-		$scope.validChecked = false;
+		// $scope.validChecked = false;
 		$scope.bussinessTypes = businessType;
-		$scope.firstName = 'Test';
-		$scope.lastName = 'Test';
-		$scope.emailAddress = 'test@etailercredit.com';
-		$scope.businessName = 'TestCorp';
-		$scope.phoneNumber = '8005551234';
-		$scope.yearStarted = 2003;
-		$scope.estimatedFICO = 699;
-		$scope.estimatedAnnualRevenue = 99000;
-		$scope.grossPercentageFromCards = 89;
-		$scope.typeOfBusiness = 'Accounting';
+		$scope.showErrorMessage = false;
 
-		$scope.submit = function() {
-			$scope.validChecked = true;
-			// debugger
-			var formData = {
-				firstName: $scope.firstName,
-				lastName: $scope.lastName,
-				emailAddress: $scope.emailAddress,
-				businessName: $scope.businessName,
-				phoneNumber: $scope.phoneNumber,
-				yearStarted: $scope.yearStarted,
-				estimatedFICO: $scope.estimatedFICO,
-				estimatedAnnualRevenue: $scope.estimatedAnnualRevenue,
-				grossPercentageFromCards: $scope.grossPercentageFromCards,
-				typeOfBusiness: $scope.typeOfBusiness
-			};
-			dataService.prequalify(formData).then(function(result) {
-				if (result.data.Qualified) {
-					ModalService.showModal({
-						templateUrl: '/components/modal/modalSuccess.html',
-						controller: function() {
-							this.applicant = $scope.firstName + ' ' + $scope.lastName;
-							this.qualifiedAmount = result.data.QualifyAmount;
-							this.redirectURL = result.data.RedirectUrl;
-						},
-						controllerAs: "modalSuccess"
-					}).then(function(modal) {
-						modal.element.modal();
-						modal.close.then(function(result) {
+		// $scope.firstName = 'Test';
+		// $scope.lastName = 'Test';
+		// $scope.emailAddress = 'test@etailercredit.com';
+		// $scope.businessName = 'TestCorp';
+		// $scope.phoneNumber = '8005551234';
+		// $scope.yearStarted = 2003;
+		// $scope.estimatedFICO = 699;
+		// $scope.estimatedAnnualRevenue = 100;
+		// $scope.grossPercentageFromCards = 10;
+		// $scope.typeOfBusiness = 'Accounting';
+
+		$scope.firstName = '';
+		$scope.lastName = '';
+		$scope.emailAddress = '';
+		$scope.businessName = '';
+		$scope.phoneNumber = '';
+		$scope.yearStarted = null;
+		$scope.estimatedFICO = null;
+		$scope.estimatedAnnualRevenue = null;
+		$scope.grossPercentageFromCards = null;
+		$scope.typeOfBusiness = '';
+
+		$scope.submit = function(isValid) {
+			if (isValid) {
+				$scope.showErrorMessage = false;
+				var formData = {
+					firstName: $scope.firstName,
+					lastName: $scope.lastName,
+					emailAddress: $scope.emailAddress,
+					businessName: $scope.businessName,
+					phoneNumber: $scope.phoneNumber,
+					yearStarted: $scope.yearStarted,
+					estimatedFICO: $scope.estimatedFICO,
+					estimatedAnnualRevenue: $scope.estimatedAnnualRevenue,
+					grossPercentageFromCards: $scope.grossPercentageFromCards,
+					typeOfBusiness: $scope.typeOfBusiness
+				};
+				dataService.prequalify(formData).then(function(result) {
+					if (result.data.Qualified) {
+						ModalService.showModal({
+							templateUrl: '/components/modal/modalSuccess.html',
+							controller: function() {
+								this.applicant = $scope.firstName + ' ' + $scope.lastName;
+								this.qualifiedAmount = result.data.QualifyAmount;
+								this.redirectURL = result.data.RedirectUrl;
+							},
+							controllerAs: "modalSuccess"
+						}).then(function(modal) {
+							modal.element.modal();
+							modal.close.then(function(result) {});
 						});
-					});
-				} else {
-					ModalService.showModal({
-						templateUrl: '/components/modal/modalFail.html'
-					}).then(function(modal) {
-						modal.element.modal();
-						modal.close.then(function(result) {
+					} else {
+						ModalService.showModal({
+							templateUrl: '/components/modal/modalFail.html',
+							controller: function() {
+
+							}
+						}).then(function(modal) {
+							modal.element.modal();
+							modal.close.then(function(result) {});
 						});
-					});
-				}
-			});
+					}
+				});
+			} else {
+				$scope.showErrorMessage = true;
+				return;
+			}
+
 		};
 	});
---
+++ client/directives/numbersOnly.directive.js
@@ -0,0 +1,21 @@
+'use strict';
+// for text input, only number is allowed,, dot is also not allowed
+angular.module('app.directives').directive('numbersOnly', function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, element, attrs, modelCtrl) {
+            modelCtrl.$parsers.push(function(inputValue) {
+                if (inputValue === undefined) {
+                    return '';
+                }
+                var transformedInput = inputValue.replace(/[^0-9]/g, '');
+                if (transformedInput !== inputValue) {
+                    modelCtrl.$setViewValue(transformedInput);
+                    modelCtrl.$render();
+                }
+
+                return transformedInput;
+            });
+        }
+    };
+});
